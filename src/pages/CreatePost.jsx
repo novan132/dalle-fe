dd061a637a6f0147012a3357c3743d339d1b0c89
Novan <l4novan132@gmail.com>
2023-08-08 14:49:01 +0700

    feat: implement layout for create post page

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,4 +1,55 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
+import { FormField, Loader } from '../components';
+
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+  const handleChange = (e) => {};
+  const handleSupriseMe = () => {};
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
+          Create stunning images generated through Dalle AI and share!
+        </p>
+      </div>
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            labelName="Your Name"
+            type="text"
+            name="name"
+            placeholder="Jane Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="'a surrealist dream-like oil painting by Salvador Dalí of a cat playing checkers'"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSupriseMe={handleSupriseMe}
+          />
+        </div>
+      </form>
+    </section>
+  );
 };
 export default CreatePost;
