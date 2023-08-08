a07aa429d0a5725ee13b253a45a785ee65502020
Novan <l4novan132@gmail.com>
2023-08-09 06:46:26 +0700

    feat: implement submit form

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -38,7 +38,31 @@ const CreatePost = () => {
     }
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (form.prompt && form.photo) {
+      setLoading(true);
+
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/posts', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(form),
+        });
+
+        await response.json();
+        navigate('/');
+      } catch (error) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please enter prompt and generate image');
+    }
+  };
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
