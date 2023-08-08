fb962e61933d1a229cea1036e618ea1de2a08665
Novan <l4novan132@gmail.com>
2023-08-08 17:17:13 +0700

    create layout for create post

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -15,6 +15,8 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const generateImage = () => {};
+
   const handleSubmit = () => {};
   const handleChange = (e) => {};
   const handleSupriseMe = () => {};
@@ -41,12 +43,53 @@ const CreatePost = () => {
             labelName="Prompt"
             type="text"
             name="prompt"
-            placeholder="'a surrealist dream-like oil painting by Salvador Dalí of a cat playing checkers'"
+            placeholder="a surrealist dream-like oil painting by Salvador Dalí of a cat playing checkers"
             value={form.prompt}
             handleChange={handleChange}
             isSurpriseMe
             handleSupriseMe={handleSupriseMe}
           />
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {generatingImg ? 'Generating...' : 'Generate'}
+          </button>
+        </div>
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">
+            Once you have created the image, you can share it with others
+          </p>
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md
+          text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? 'Sharing...' : 'Share with community'}
+          </button>
         </div>
       </form>
     </section>
